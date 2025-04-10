28f9c140c5c457bc2ea10b238e6b834f1343d678
jianming zheng <jianmiz1@uci.edu>
2025-04-10 09:36:09 -0700

    4th attempt

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+SYSROOT=$(aarch64-none-linux-gnu-gcc -print-sysroot)
 
 if [ $# -lt 1 ]
 then
@@ -23,7 +24,12 @@ fi
 
 mkdir -p ${OUTDIR}
 
-cd "$OUTDIR"    
+if [ ! -d "${OUTDIR}" ];
+then
+    exit 1
+fi
+
+cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
@@ -34,16 +40,11 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    echo "Building kernel"
-    # clean 
+    # TODO: Add your kernel build steps here
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-    # configure
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    # build a kernel image
     make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    # modules 
-    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
-    # build the device tree
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
@@ -58,13 +59,12 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# Create necessary base directoriesm
+# Create necessary base directories
 mkdir -p rootfs
 cd rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
-
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -72,33 +72,32 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
+    # Configure busybox
     make distclean
     make defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 else
     cd busybox
 fi
 
 # Make and install busybox
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
-
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
-
 # Add library dependencies to rootfs
-SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
-
 cd ${OUTDIR}/rootfs
 cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
 cp ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
 cp ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
 cp ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
 
+echo "Library dependencies"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+
 # Make device nodes
-sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
-sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
+cd ${OUTDIR}/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
 # Clean and build the writer utility
 cd ${FINDER_APP_DIR}
@@ -119,7 +118,6 @@ sudo chown -R root:root ${OUTDIR}/rootfs
 
 # Create initramfs.cpio.gz
 cd ${OUTDIR}/rootfs
-
-find . | cpio -H newc -ov --owner root:root>${OUTDIR}/initramfs.cpio
-
-gzip -f ${OUTDIR}/initramfs.cpio
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
