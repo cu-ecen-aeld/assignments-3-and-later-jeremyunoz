68d3d1b23fceb774f6c4ba31b6e9b8101c9ca4c0
jianming zheng <jianmiz1@uci.edu>
2025-04-09 19:10:21 -0700

    attempt assignment-3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -41,7 +41,6 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     # build a kernel image
     make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
     # build the device tree
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
@@ -57,13 +56,7 @@ then
 fi
 
 # Create necessary base directoriesm
-mkdir -p rootfs
-cd rootfs
-
-mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-mkdir -p usr/bin usr/lib usr/sbin
-mkdir -p var/log
-mkdir -p home/conf
+mkdir -p "$OUTDIR"/rootfs/{bin,dev,etc,home/conf,lib,lib64,proc,sbin,sys,tmp,usr/{bin,lib,sbin},var/log}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -80,45 +73,43 @@ fi
 # Make and install busybox
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
-cd ${OUTDIR}/rootfs
+
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-SYSROOT=`${CROSS_COMPILE}gcc --print-sysroot`
-echo sysroot is $SYSROOT
+SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
+echo "${SYSROOT}"
 
-cp $SYSROOT/lib/ld-linux-aarch64.so.1 lib/
-cp $SYSROOT/lib64/libm.so.6 lib64/
-cp $SYSROOT/lib64/libresolv.so.2 lib64/
-cp $SYSROOT/lib64/libc.so.6 lib64/
+cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+cp -a ${SYSROOT}/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
+cp -a ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+cp -a ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
 
 # Make device nodes
-cd "${OUTDIR}/rootfs"
-sudo mknod -m 666 dev/null c 1 3
-sudo mknod -m 600 dev/console c 5 1
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1
 
 # Clean and build the writer utility
-make -C ${FINDER_APP_DIR} clean CROSS_COMPILE=${CROSS_COMPILE}
-make -C ${FINDER_APP_DIR} all CROSS_COMPILE=${CROSS_COMPILE}
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-cp ${FINDER_APP_DIR}/finder.sh home/
-cp ${FINDER_APP_DIR}/finder-test.sh home/
-cp ${FINDER_APP_DIR}/autorun-qemu.sh home/
-cp ${FINDER_APP_DIR}/writer home/
-cp ${FINDER_APP_DIR}/start-qemu-app.sh .
-cp ${FINDER_APP_DIR}/../conf/username.txt home/conf/
-cp ${FINDER_APP_DIR}/../conf/assignment.txt conf/
+cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
+cp -r ${FINDER_APP_DIR}/../conf ${OUTDIR}/rootfs/home/
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home/
 
 # TODO: Chown the root directory
-sudo chown -R root:root *
+chown -R root:root ${OUTDIR}/rootfs
 
 # TODO: Create initramfs.cpio.gz
-"${OUTDIR}/rootfs"
-find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root>${OUTDIR}/initramfs.cpio
 
-gzip -f initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
