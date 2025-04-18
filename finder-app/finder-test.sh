3f1440df371a51d00eb8942a9d12fbd5f33b1e43
jianming zheng <jianmiz1@uci.edu>
2025-04-18 16:34:08 -0700

    modified finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,14 +8,15 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
+ASSIGNMENT_OUTPUT="/tmp/assignment4-result.txt"
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
+	echo "Using default value ${WRITESTR} for string to write" >> ${ASSIGNMENT_OUTPUT}
 	if [ $# -lt 1 ]
 	then
-		echo "Using default value ${NUMFILES} for number of files to write"
+		echo "Using default value ${NUMFILES} for number of files to write" >> ${ASSIGNMENT_OUTPUT}
 	else
 		NUMFILES=$1
 	fi	
@@ -27,12 +28,12 @@ fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
 
-echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
+echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}" >> ${ASSIGNMENT_OUTPUT}
 
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -43,32 +44,31 @@ then
 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
 	if [ -d "$WRITEDIR" ]
 	then
-		echo "$WRITEDIR created"
+		echo "$WRITEDIR created" >> ${ASSIGNMENT_OUTPUT}
 	else
 		exit 1
 	fi
 fi
-
-echo "Removing the old writer utility and compiling as a native application"
-# make clean
-# make
+#echo "Removing the old writer utility and compiling as a native application"
+#make clean
+#make
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	/usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+echo ${OUTPUTSTRING} | grep "${MATCHSTR}" >> ${ASSIGNMENT_OUTPUT}
 if [ $? -eq 0 ]; then
-	echo "success"
+	echo "success" >> ${ASSIGNMENT_OUTPUT}
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found" >> ${ASSIGNMENT_OUTPUT}
 	exit 1
 fi
