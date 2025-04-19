5311de25b3da477a1170299e2ddc89c55337a6c6
jianming zheng <jianmiz1@uci.edu>
2025-04-19 12:28:31 -0700

    modified assignment out redirection

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -13,10 +13,10 @@ ASSIGNMENT_OUTPUT="/tmp/assignment4-result.txt"
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write" >> ${ASSIGNMENT_OUTPUT}
+	echo "Using default value ${WRITESTR} for string to write" 
 	if [ $# -lt 1 ]
 	then
-		echo "Using default value ${NUMFILES} for number of files to write" >> ${ASSIGNMENT_OUTPUT}
+		echo "Using default value ${NUMFILES} for number of files to write" 
 	else
 		NUMFILES=$1
 	fi	
@@ -28,7 +28,7 @@ fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
 
-echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}" >> ${ASSIGNMENT_OUTPUT}
+echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}" 
 
 rm -rf "${WRITEDIR}"
 
@@ -44,7 +44,7 @@ then
 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
 	if [ -d "$WRITEDIR" ]
 	then
-		echo "$WRITEDIR created" >> ${ASSIGNMENT_OUTPUT}
+		echo "$WRITEDIR created" 
 	else
 		exit 1
 	fi
@@ -58,17 +58,17 @@ do
 	/usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR") >> ${ASSIGNMENT_OUTPUT}
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}" >> ${ASSIGNMENT_OUTPUT}
+echo ${OUTPUTSTRING} | grep "${MATCHSTR}" 
 if [ $? -eq 0 ]; then
-	echo "success" >> ${ASSIGNMENT_OUTPUT}
+	echo "success" 
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found" >> ${ASSIGNMENT_OUTPUT}
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found" 
 	exit 1
 fi
